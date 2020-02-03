e949da88298c2a7ef6e4c41e5b930caaa71132ef
foliwe <foliwe@gmail.com>
2020-02-03 13:32:56 +0000

    minior changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         before_save { email.downcase! } 
          validates :full_name ,presence: true, length: {minimum:5, maximum:50}
 end
