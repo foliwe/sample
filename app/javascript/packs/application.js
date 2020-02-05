afd1c1cf4ef8e9c4a811ffc7de92179d02c3f659
foliwe <foliwe@gmail.com>
2020-02-05 01:01:29 +0000

    inner UI edit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,15 +9,16 @@ require("@rails/activestorage").start()
 require("channels")
 window.Noty = require("noty")
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import {
+    library,
+    dom
+} from '@fortawesome/fontawesome-svg-core'
+import {
+    fas
+} from '@fortawesome/free-solid-svg-icons'
+import {
+    faTwitter
+} from '@fortawesome/free-brands-svg-icons'
 library.add(fas, faTwitter) // Kicks off the process of finding <i> tags and replacing with <svg>
 dom.watch()
 import 'bootstrap'
@@ -28,4 +29,12 @@ import './stylesheets/application.scss'
 document.addEventListener("turbolinks:load", () => {
     $('[data-toggle="tooltip"]').tooltip()
     $('[data-toggle="popover"]').popover()
+})
+
+$(document).ready(() => {
+    $('.toggle').on('click', (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        $('#' + e.target.getAttribute('aria-controls')).toggleClass('d-none');
+    })
 })
